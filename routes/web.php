c29440f4a54082ad98d7f9c462865fa509786e14
David Tang <dtang@auditboard.com>
2020-02-09 21:06:29 -0800

    2.5 - Deleting an Album

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ Route::get('/invoices/{id}', 'InvoiceController@show');
 Route::get('/albums', 'AlbumController@index')->name('albums');
 Route::get('/albums/create', 'AlbumController@create');
 Route::post('/albums', 'AlbumController@store');
+Route::get('/albums/{id}/delete', 'AlbumController@deleteConfirmation');
+Route::post('/albums/{id}/delete', 'AlbumController@destroy');
